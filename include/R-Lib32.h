d312ad73238220d9dbc759378583f2b40dc53ff6
Tim Fischbach <59933715+timfischbach@users.noreply.github.com>
2023-08-13 19:32:51 +0200

    Add files via upload

--- include/R-Lib32.h
+++ include/R-Lib32.h
@@ -2,49 +2,53 @@
 #define RLib32
 
 #include <Arduino.h>
-#include <WiFi.h>
-#include <HTTPClient.h>
-#include <Update.h>
-#include <WebServer.h>
-#include <EEPROM.h>
-#include "R-Lib32.h"
 
-extern const String LIBVERSION;
+class R_Lib32
+{
+public:
+    R_Lib32();
+    String checkUpdate();
+    String split(String s, char parser, int index);
+    String performUpdate();
+    void connectWIFI();
+    bool checkWIFI();
+    void resetWIFI();
+    void saveWIFI(String ssid, String password);
+    void connectWIFIUser(String ssid, String password);
+    void createWebServer();
+    String getLibVersion();
+    void connectWIFIUserHandle();
+    void endWIFIUser();
+    void setDeviceName(String DevName);
+    String getDeviceName();
+    void setVersion(String ver);
+    String getVersion();
+    void setDlLink(String DLL);
+    String getDlLink();
+    void setBetaState(bool sbeta);
+    bool getBetaState();
+    void setDevState(bool sdev);
+    bool getDevState();
+    bool varCheck();
+    void saveOV(String oldversion);
+    String loadOV();
+    void updateStarted();
+    void updateFinished();
+    void updateProgress(int cur, int total);
+    void updateError(int err);
+    void setSSLRootCertificate(char *certificate);
+    bool getSSLState();
+    void setSSLState(bool sssl);
+    void setSSLInsecureState(bool sinsecure);
+    bool getSSLInsecureState();
+    void SSLSetup();
+    String dataTransmission();
+    void setAttemptsBeforeInsecureSSL(int sattempts);
+    int getAttemptsBeforeInsecureSSL();
+    void executeAttemptsBeforeInsecureSSL();
 
-extern int progress;
+private:
 
-String checkUpdate();
-String split(String s, char parser, int index);
-String performUpdate();
-String dataTransmission();
-void connectWIFI(String ssid, String passwd);
-bool checkWIFI();
-void resetWIFI();
-void disconnectWIFI();
-void saveWIFI(String ssid, String password);
-String loadWIFI(String mode);
-void connectWIFIUser(String ssid, String password);
-void createWebServer();
-String getLibVersion();
-void connectWIFIUserHandle();
-void endWIFIUser();
-String getLibVersion();
-void connectWIFIUserHandle();
-void endWIFIUser();
-void setDeviceName(String DevName);
-String getDeviceName();
-void setVersion(String ver);
-String getVersion();
-void setDlLink(String DLL);
-String getDlLink();
-void setDevLink(String DEL);
-String getDevLink();
-void setDevTag(String DTAG);
-String getDevTag();
-void setBetaState(bool sbeta);
-bool getBetaState();
-void setDevState(bool sdev);
-bool getDevState();
-bool varCheck();
+};
 
 #endif
